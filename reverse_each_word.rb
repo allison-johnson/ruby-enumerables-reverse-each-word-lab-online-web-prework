b52cc5894a1f1dd99bf4d4c2db648a89d65cc593
Allison Johnson <allison.cuttler@gmail.com>
2019-10-08 16:43:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
 def reverse_each_word(sentence)
-  words = %w[sentence]
+  words = sentence.split(" ") #Array of all the words in sentence
+  
+  #Now eliminate any puncutation...
+  counter = 0
+  while counter < words.length do
+    words[counter].gsub(/\W/,'')
+    counter += 1
+  end #end while
 end
